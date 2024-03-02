05c3a40df0159f60091d1dca476c31e08b3f19a9
Enrico Ros <enrico.ros@gmail.com>
2024-03-02 03:12:29 -0800

    fix build (really)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import {Suspense} from "react";
 import TwitterList from "@/components/twitter-list";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-black">
-      <TwitterList/>
+      <Suspense>
+        <TwitterList/>
+      </Suspense>
     </main>
   );
 }
