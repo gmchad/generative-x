82cd55ffcc2d560dee60434bb040d239c1e70a31
chadlohrli <chad.david.lohrli@gmail.com>
2024-03-01 23:51:24 -0800

    update: list ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TwitterList from "@/components/twitter-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between mt-2 bg-black">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-black">
       <TwitterList/>
     </main>
   );
