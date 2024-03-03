79a8f4ae98f0d0c8bc6705c137318422b9097edf
Enrico Ros <enrico.ros@gmail.com>
2024-03-03 13:11:49 -0800

    update skel

--- components/dynamicskeleton.tsx
+++ components/dynamicskeleton.tsx
@@ -3,14 +3,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function DynamicSkeleton() {
 	return (
 		<div className="flex border-b border-gray-700 p-4 bg-black">
-			<div className="flex flex-col">
-				<div className="flex items-center">
-					<Skeleton className="h-5 w-72 rounded-md" />
+			{/*<div className="flex flex-col">*/}
+				{/*<div className="flex items-center">*/}
+				{/*	<Skeleton className="h-5 w-72 rounded-md" />*/}
+				{/*</div>*/}
+				<div className="flex items-center w-72 ">
+					<Skeleton className="h-48 w-72 rounded-md" />
 				</div>
-				<div className="mt-2">
-					<Skeleton className="h-48 w-full rounded-lg" />
-				</div>
-			</div>
+			{/*</div>*/}
 		</div>
 	);
 }
