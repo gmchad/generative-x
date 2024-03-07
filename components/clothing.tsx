5145d667b17fd33157f7fba3d15f300f184660dc
chadlohrli <chad.david.lohrli@gmail.com>
2024-03-07 13:50:37 -0800

    update: remove client side UI from clothing component

--- components/clothing.tsx
+++ components/clothing.tsx
@@ -33,18 +33,18 @@ const TwitterCard: React.FC<TwitterCardProps> = ({
         <img
           src={img_src}
           alt={item_type}
-          style={{ width: "100%", height: "auto", cursor: "pointer" }}
+          className="h-full w-full cursor-pointer object-cover"
         />
       </a>
-      {/* <div>
-        <p>{description}</p>
+      <div>
+        {/* <p>{description}</p> */}
         <p>Type: {item_type}</p>
-      </div> */}
+      </div>
     </div>
   );
 };
 
-interface ClothingProps {
+export interface ClothingProps {
   data: {
     [category: string]: {
       img_src: string;
@@ -62,40 +62,22 @@ export const Clothing = ({ props }: { props: ClothingProps }) => {
       <CardHeader className="flex-col items-start">
         <CardTitle className="text-center text-2xl">Get This Outfit</CardTitle>
       </CardHeader>
-      <CardContent className="grid gap-1.5">
-        <div className={`flex items-center justify-start`}>
-          {/* <span className="text-sm justify-center">Political Leaning: </span> */}
-          {/* <span className={`text-xl font-semibold ${colorClass} ml-1`}>{props.party}</span> */}
-        </div>
-
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-            gap: "20px",
-          }}
-        >
-          <Carousel className="w-full max-w-xs">
-            <CarouselContent className="overflow-visible">
-              {Object.entries(props)
-                .filter(([category, items]) => items.length > 0)
-                .map(([category, items], index) => (
-                  <div key={index}>
-                    <div className="grid gap-4 px-1.5">
-                      <div className="flex items-center">
-                        <div className="shadcn-accent-pill rounded-full text-lg text-black"></div>
-                        <h2 style={{ textAlign: "center" }}>{category}</h2>
-                        {items.map((item: any, index: number) => (
-                          <TwitterCard key={index} {...item} />
-                        ))}
-                      </div>
-                    </div>
+      <CardContent className="overflow-x-auto">
+        <div className="flex space-x-4 py-2">
+          {Object.entries(props)
+            .filter(([category, items]) => items.length > 0)
+            .map(([category, items], index) => (
+              <div key={index} className="min-w-[200px]">
+                <div className="flex flex-col items-center space-y-4">
+                  <div className="text-lg text-black">
+                    <h2 className="text-center">{category}</h2>
                   </div>
-                ))}
-            </CarouselContent>
-            <CarouselPrevious className="max-w-sm" />
-            <CarouselNext className="max-w-sm" />
-          </Carousel>
+                  {items.map((item: any, index: number) => (
+                    <TwitterCard key={index} {...item} />
+                  ))}
+                </div>
+              </div>
+            ))}
         </div>
       </CardContent>
     </Card>
